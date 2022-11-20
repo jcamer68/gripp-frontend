70d1547d42b93abcd87dd67bd6eb8b9f4851f389
Myles MacDonald <mylesmacdonald@Myless-MacBook-Air.local>
2022-11-20 09:57:53 -0500

    last

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({ name, stat, details }) => {
                     <div className="e-card-header-title"> {name}</div>
                 </div>
             </div>
-            <div className="e-card-content">{stat}th</div>
+            <div className="e-card-content" style={{color: "#837dff"}}>{stat}th</div>
             <div className="e-card-header">
                 <div className="e-card-header-caption">
                     <div className="e-card-header-subtitle"> {details}</div>

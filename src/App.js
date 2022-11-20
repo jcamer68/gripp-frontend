b47d5ff85dd544ff7ae4ce5bdf9f9fbd8d021582
John Cameron <jchen.cameron01@gmail.com>
2022-11-19 21:35:12 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // new line start
+  const [profileData, setProfileData] = useState(null);
+
+  function getData() {
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:5000/user/1",
+    })
+      .then((response) => {
+        const res = response.data;
+        console.log(response);
+        console.log("hi");
+        console.log(res);
+        setProfileData({
+          profile_name: res.data.name,
+          about_me: res.data.age,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+  //end of new line
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +47,17 @@ function App() {
         >
           Learn React
         </a>
+
+        {/* new line start*/}
+        <p>To get your profile details: </p>
+        <button onClick={getData}>Click me</button>
+        {profileData && (
+          <div>
+            <p>Profile name: {profileData.profile_name}</p>
+            <p>About me: {profileData.about_me}</p>
+          </div>
+        )}
+        {/* end of new line */}
       </header>
     </div>
   );

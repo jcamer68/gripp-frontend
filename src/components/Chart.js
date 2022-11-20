cfa7a89344d33fd1b4253bbcb9fe8bf0d83acf82
Myles MacDonald <mylesmacdonald@dyn172-30-37-106.wireless.uwo.pri>
2022-11-19 22:48:16 -0500

    chart

---
+++ src/components/Chart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Category, ChartComponent, ColumnSeries, Inject, LineSeries, SeriesCollectionDirective, SeriesDirective, Tooltip } from '@syncfusion/ej2-react-charts';
+
+const Chart = ({data, xname, yname, name}) => {
+    const primaryxAxis = { valueType: 'Category' };
+    return <ChartComponent id="charts" primaryXAxis={primaryxAxis}>
+        <Inject services={[ColumnSeries, Tooltip, LineSeries, Category]} />
+        <SeriesCollectionDirective>
+            <SeriesDirective dataSource={data} xName={xname} yName={yname} name={name} />
+        </SeriesCollectionDirective>
+    </ChartComponent>;
+};
+
+export default Chart;
